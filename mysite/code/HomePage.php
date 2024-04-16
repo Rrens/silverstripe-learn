04c9cf53de3f0b34c6e83c27a9964a47e0a2c3ef
Rrens <yusufrendi313@gmail.com>
2024-04-16 13:37:19 +0700

    Play w/ data relationship (has many)

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -6,4 +6,10 @@ class HomePage extends Page
 
 class HomePage_Controller extends Page_Controller
 {
+    public function LatestArticles($limit = null)
+    {
+        return ArticlePage::get()
+            ->sort('Created', 'DESC')
+            ->limit($limit);
+    }
 }
