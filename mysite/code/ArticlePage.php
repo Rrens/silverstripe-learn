efc1273772c1e1189fae5a91674dbe9bcb543ac7
Rrens <yusufrendi313@gmail.com>
2024-04-16 15:30:56 +0700

    Form handle

--- mysite/code/ArticlePage.php
+++ mysite/code/ArticlePage.php
@@ -20,6 +20,10 @@ class ArticlePage extends Page
         'Categories' => 'ArticleCategory'
     );
 
+    private static $has_many = array(
+        'Comments' => 'ArticleComment'
+    );
+
 
     private static $can_be_root = false;
 
@@ -65,4 +69,58 @@ class ArticlePage extends Page
 
 class ArticlePage_Controller extends Page_Controller
 {
+
+    private static $allowed_actions = array(
+        'CommentForm',
+    );
+
+    public function CommentForm()
+    {
+        $form = Form::create(
+            $this,
+            __FUNCTION__,
+            FieldList::create(
+                TextField::create('Name', ''),
+                EmailField::create('Email', ''),
+                TextAreaField::create('Comment', ''),
+
+            ),
+            FieldList::create(
+                FormAction::create('HandleComment', 'Post Comment')
+                    ->setUseButtonTag(true)
+                    ->addExtraClass('btn btn-dafault-color btn-lg')
+            ),
+            RequiredFields::create('Name', 'Email', 'Comment')
+        )->addExtraClass('form-style')->addExtraClass('col-sm-12');
+
+        foreach ($form->Fields() as $field) {
+            $field->addExtraClass('form-control')
+                ->setAttribute('placeholder', $field->getName() . '*');
+        }
+
+        $data = Session::get("FormData.{$form->getName()}.data");
+
+        return $data ? $form->loadDataFrom($data) : $form;
+    }
+
+    public function handleComment($data, $form)
+    {
+        Session::set("FormData.{$form->getName()}.data", $data);
+        $existing = $this->Comments()->filter(array(
+            'Comment' => $data['Comment'],
+        ));
+
+        if ($existing->exists() && strlen($data['Comment']) > 20) {
+            $form->sessionMessage('You have already posted this comment', 'bad');
+            return $this->redirectBack();
+        }
+        $comment = ArticleComment::create();
+        $comment->ArticlePageID = $this->ID;
+        $form->saveInto($comment);
+        $comment->write();
+
+        Session::clear("FormData.{$form->getName()}.data");
+        $form->sessionMessage('Thanks for your comment', 'good');
+        return $this->redirectBack();
+    }
 }
