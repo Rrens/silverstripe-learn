44b21b57e25b30710ea0905e9f7f5ebf6558c798
Rrens <yusufrendi313@gmail.com>
2024-04-16 09:13:25 +0700

    Layouting Frontend

--- mysite/code/Page_Controller.php
+++ mysite/code/Page_Controller.php
@@ -17,13 +17,41 @@ class Page_Controller extends ContentController
      *
      * @var array
      */
-    private static $allowed_actions = array(
-    );
+    private static $allowed_actions = array();
 
     public function init()
     {
         parent::init();
         // You can include any CSS or JS required by your project here.
         // See: http://doc.silverstripe.org/framework/en/reference/requirements
+
+        Requirements::css("http://fonts.googleapis.com/css?family=Raleway:300,500,900%7COpen+Sans:400,700,400italic");
+        Requirements::css("{$this->ThemeDir()}/static/css/bootstrap.min.css");
+        Requirements::css("{$this->ThemeDir()}/static/css/style.css");
+
+        Requirements::javascript("{$this->ThemeDir()}/static/js/common/modernizr.js");
+        Requirements::javascript("{$this->ThemeDir()}/static/js/common/jquery-1.11.1.min.js");
+        Requirements::javascript("{$this->ThemeDir()}/static/js/common/bootstrap.min.js");
+        Requirements::javascript("{$this->ThemeDir()}/static/js/common/bootstrap-datepicker.js");
+        Requirements::javascript("{$this->ThemeDir()}/static/js/common/chosen.min.js");
+        Requirements::javascript("{$this->ThemeDir()}/static/js/common/bootstrap-checkbox.js");
+        Requirements::javascript("{$this->ThemeDir()}/static/js/common/nice-scroll.js");
+        Requirements::javascript("{$this->ThemeDir()}/static/js/common/jquery-browser.js");
+        Requirements::javascript("{$this->ThemeDir()}/static/js/scripts.js");
+
+
+
+        //     <!-- Google Web Font -->
+        // <link href="http://fonts.googleapis.com/css?family=Raleway:300,500,900%7COpen+Sans:400,700,400italic"
+        //     rel="stylesheet" type="text/css" />
+
+        // <!-- Bootstrap CSS -->
+        // <link href="$ThemeDir/static/css/bootstrap.min.css" rel="stylesheet" />
+
+        // <!-- Template CSS -->
+        // <link href="$ThemeDir/static/css/style.css" rel="stylesheet" />
+
+        // <!-- Modernizr -->
+        // <script src="$ThemeDir/static/js/common/modernizr.js"></script>
     }
 }
